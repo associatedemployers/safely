80d0f1ac5d8b73e36de0ef09f58706f2a452f576
James-Mac <james@aehr.org>
2016-12-13 15:30:19 -0700

    Adding a select

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 import authenticated from 'safely/mixins/authenticated';
 
-const { Route } = Ember;
+const { Route, RSVP } = Ember;
 
-export default Route.extend(authenticated, {});
+export default Route.extend(authenticated, {
+  model () {
+    return RSVP.hash({
+      classes: this.store.findAll('class')
+    });
+  },
+
+  setupController (controller, model) {
+    controller.setProperties({
+      classes: model.classes
+    });
+  }
+});
