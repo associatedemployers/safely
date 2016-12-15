592a7660d49040d35b0e7085e492ddc54a4354fc
J Scott Chapman <scott8675309@gmail.com>
2016-12-15 15:36:52 -0700

    Intermediate commit for calendar display.

--- app/components/calendar/x-block.js
+++ app/components/calendar/x-block.js
@@ -5,19 +5,46 @@ const { Component, computed } = Ember;
 
 export default Component.extend({
   tagName: 'button',
-  classNames: [ 'cal__avail-button', 'button', 'is-outlined' ],
-  classNameBindings: [ 'block.lowSeats:is-warning:is-dark', 'inActiveBlocks:cal__avail-button--active' ],
+  classNames: [ 'cal__avail-button', 'button', 'is-outlined'],
+  classNameBindings: [
+    'block.lowSeats:is-warning:is-dark',
+    'inActiveBlocks:cal__avail-button--active',
+    'unsuitable:cal__avail-button--unsuitable',
+    'noSeats:cal__avail-button--no-seats',
+    'loading:is-loading'
+  ],
+
+  loading: computed.and('hover', 'loadingState'),
+  noSeats: computed.equal('block.information.seats', 0),
 
   mouseEnter () {
+    if ( this.get('hover') ) {
+      return;
+    }
+
+    this.set('hover', true);
     this.get('focusBlock')(this.get('block.originalBlock'), this.get('date'));
   },
 
   mouseLeave () {
+    this.set('hover', false);
     this.get('unfocusBlock')(this.get('block.originalBlock'));
+  },
+
+  click () {
+    let date = moment(this.get('date')).hour(this.get('block.originalBlock')[0]).toDate();
+    this.get('onClick')(date);
   },
 
   dateString: computed('date', function () {
     return moment(this.get('date')).format('M/D/YY');
+  }),
+
+  toolTipMessage: computed('registerWarning', 'unsuitable', function () {
+    let registerWarning = this.get('registerWarning'),
+        unsuitable = this.get('unsuitable');
+
+    return unsuitable ? 'Unsuitable block for your requirements. Please try another' : registerWarning ? registerWarning : 'Click to Book';
   }),
 
   inActiveBlocks: computed('activeBlocks.[]', 'block.originalBlock.[]', 'date', function () {
@@ -28,5 +55,9 @@ export default Component.extend({
     return blocks ? blocks.find(a => {
       return a.date === date && a.block[0] === b[0] && a.block[1] === b[1];
     }) : false;
+  }),
+
+  unsuitable: computed('inActiveBlocks', 'hover', function () {
+    return !this.get('inActiveBlocks') && this.get('hover') ? true : false;
   })
 });
