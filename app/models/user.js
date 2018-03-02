c88933af3fbbd186a358b5171585d9f348f30275
James <james@aehr.org>
2018-03-02 14:13:25 -0700

    Add company to rel model

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,12 @@
 import DS from 'ember-data';
 import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   name: attr(),
   email: attr('string'),
   administrative: attr('boolean'),
   activatedOn: attr('date'),
-  password: attr('string')
+  password: attr('string'),
+  company: belongsTo('company', { async: true, inverse: null })
 });
