b00c1f724f60fe1af89ba179f733be4fd7f25329
James <james@aehr.org>
2020-02-25 07:30:39 -0700

    Sync up prog

--- app/models/hub-instructor.js
+++ app/models/hub-instructor.js
@@ -1,8 +1,7 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class HubInstructorModel extends Model {
-  @attr('string') firstName
-  @attr('string') lastName
+  @attr('string') displayName
   @attr('string') email
   @attr('number') phone
   @attr('string') location
